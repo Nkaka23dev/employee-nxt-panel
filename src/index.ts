9d18efeab702e473454243fbab18121b545cf5c8
ericnkaka <nkakaeric96@gmail.com>
2024-08-07 15:02:42 +0200

    chore: project setup

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+
+const server = new ApolloServer({
+  typeDefs: "",
+  resolvers: {},
+});
+
+const { url } = await startStandaloneServer(server, {
+  listen: { port: 80 },
+});
+
+console.log(`Server is running at ${url}`);
